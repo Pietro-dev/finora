e29e1ccc6f6b8e0be71de799f87b8b1a474314cf
Pietro-dev <pietro.pietromartins@gmail.com>
2025-03-19 18:00:21 -0300

    aplicação da arquitetura limpa a usuario service

--- src/main/java/com/finora/finora_backend/services/UsuarioService.java
+++ src/main/java/com/finora/finora_backend/services/UsuarioService.java
@@ -1,6 +1,6 @@
 package com.finora.finora_backend.services;
 
-import com.finora.finora_backend.repository.UsuarioRepository;
+import com.finora.finora_backend.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
