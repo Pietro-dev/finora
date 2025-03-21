fcbebf31b15399a81ee525e2526093e1defab2fe
Pietro-dev <pietro.pietromartins@gmail.com>
2025-03-20 23:28:03 -0300

    remoção da interface usuario repository impl

--- src/main/java/com/finora/finora_backend/services/UsuarioService.java
+++ src/main/java/com/finora/finora_backend/services/UsuarioService.java
@@ -9,23 +9,4 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-//    public UsuarioResponseDTO createUsuario(CreateUsuarioDTO dto) {
-//        Usuario usuario = new Usuario();
-//        usuario.setNome(dto.getNome());
-//        usuario.setEmail(dto.getEmail());
-//        usuario.setSenha(dto.getSenha()); // Senha não é exposta na resposta
-//
-//        Usuario savedUsuario = usuarioRepository.save(usuario);
-//        return new UsuarioResponseDTO(savedUsuario.getId(), savedUsuario.getNome(), savedUsuario.getEmail());
-//    }
-
-//    public Usuario updateUsuario(Long id, Usuario usuario){
-//        Usuario existingUsuario = usuarioRepository.findById(id)
-//                .orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
-//
-//        existingUsuario.setNome(usuario.getNome());
-//        existingUsuario.setEmail(usuario.getEmail());
-//
-//        return usuarioRepository.save(existingUsuario);
-//    }
 }
